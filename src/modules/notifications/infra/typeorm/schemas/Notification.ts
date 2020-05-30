f329c5af5107fe98ff7bc7b0f66a280c8a3f4d00
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2020-05-30 18:05:24 -0300

    Adicionada a configuração para utilizar o mongoDB e adicionada a entidade e repositório para Notificações.

---
+++ src/modules/notifications/infra/typeorm/schemas/Notification.ts
@@ -0,0 +1,26 @@
+import {
+  Entity, Column, ObjectIdColumn, ObjectID, CreateDateColumn, UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('notifications')
+class Notification {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  content: string;
+
+  @Column('uuid')
+  recipient_id: string;
+
+  @Column({ default: false })
+  read: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Notification;
