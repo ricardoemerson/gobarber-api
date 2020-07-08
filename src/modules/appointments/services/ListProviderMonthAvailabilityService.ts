d0aa4981ae22f1a634419fb552d7cee93fff8144
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2020-07-07 23:40:20 -0300

    Correção dos dias disponíveis no mês.

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,4 @@
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -29,9 +29,14 @@ class ListProviderMonthAvailabilityService {
     const eachDayArray = Array.from({ length: numberOfDaysInMonth }, (_, index) => index + 1);
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => getDate(appointment.date) === day);
 
-      return { day, available: appointmentsInDay.length < 10 };
+      return {
+        day,
+        available: isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
+      };
     });
 
     return availability;
